3e179a8969e402c3fa5b55ebf937c65ecb872914
Ashish Badak <ashbadak@gmail.com>
2020-10-08 00:14:49 +0530

    created StoryboardInstantiable protocol with default implementations to instantiate view controller

--- TheMovieDB/TheMovieDB/Application Navigation/StoryboardInstantiable.swift
+++ TheMovieDB/TheMovieDB/Application Navigation/StoryboardInstantiable.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol StoryboardInstantiable {
+    static var storyboardProvider: StoryboardProvider { get }
+    static func instantiate() -> Self
+}
+
+extension StoryboardInstantiable where Self: UIViewController {
+    static var storyboardIdentifier: String { String(describing: self) }
+    
+    static func instantiate() -> Self {
+        let storyboard = storyboardProvider.getStoryboard()
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: storyboardIdentifier) as? Self else {
+            fatalError("Failed to load \(storyboardIdentifier) from \(storyboardProvider.storyboardName) storyboard.")
+        }
+        return viewController
+    }
+}

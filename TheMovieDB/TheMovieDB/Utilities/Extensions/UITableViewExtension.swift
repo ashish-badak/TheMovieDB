71096052d9c754b7b1b9bb361a99618b4c9769d3
Ashish Badak <ashbadak@gmail.com>
2020-10-08 00:36:28 +0530

    added table view extension to register and dequeue table view cells

--- TheMovieDB/TheMovieDB/Utilities/Extensions/UITableViewExtension.swift
+++ TheMovieDB/TheMovieDB/Utilities/Extensions/UITableViewExtension.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITableView {
+    func register<Cell: UITableViewCell>(_ type: Cell.Type) {
+        self.register(Cell.self, forCellReuseIdentifier: Cell.reuseIdentifier)
+    }
+    
+    func register<Cell: UITableViewCell>(_ type: Cell.Type) where Cell: NibLoadableView {
+        self.register(Cell.nib, forCellReuseIdentifier: Cell.reuseIdentifier)
+    }
+    
+    func dequeue<Cell: UITableViewCell>(forIndexPath indexPath: IndexPath) -> Cell {
+        guard let cell = self.dequeueReusableCell(withIdentifier: Cell.reuseIdentifier, for: indexPath) as? Cell
+            else {
+                fatalError("Failed to dequeue cell with reuseIdentifier: \(Cell.reuseIdentifier)")
+        }
+        
+        return cell
+    }
+}

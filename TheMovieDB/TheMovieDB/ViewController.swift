7271a8136685e8e0ae42d0f2702ac6dee97a0b04
Ashish Badak <ashbadak@gmail.com>
2020-10-07 23:03:24 +0530

    created xcode project

---
+++ TheMovieDB/TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 07/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

55252f9227e179cef23f3751eee0f5a6e549f72e
Ashish Badak <ashbadak@gmail.com>
2020-10-09 01:28:43 +0530

    created DefaultRequestPerformer

--- TheMovieDB/TheMovieDB/Networking/RequestPerformer.swift
+++ TheMovieDB/TheMovieDB/Networking/RequestPerformer.swift
@@ -13,3 +13,32 @@ typealias APIRequestCallback = (Result<Data, Error>) -> Void
 protocol RequestPerformer {
     func perform(_ requestBuilder: URLRequestConvertible, then perform: @escaping APIRequestCallback)
 }
+
+final class DefaultRequestPerformer: RequestPerformer {
+    var task: URLSessionTask?
+    let responseValidator: APIResponseValidator
+    
+    init(responseValidator: APIResponseValidator) {
+        self.responseValidator = responseValidator
+    }
+    
+    convenience init() {
+        self.init(responseValidator: DefaultAPIResponseValidator())
+    }
+    
+    func perform(_ requestBuilder: URLRequestConvertible, then perform: @escaping APIRequestCallback) {
+        guard let request = requestBuilder.asURLRequest() else {
+            perform(.failure(APIRequestError.badRequest))
+            return
+        }
+        
+        task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            /// - TODO: Handle response validation and error mapping
+        }
+        task?.resume()
+    }
+    
+    func cancel() {
+        task?.cancel()
+    }
+}

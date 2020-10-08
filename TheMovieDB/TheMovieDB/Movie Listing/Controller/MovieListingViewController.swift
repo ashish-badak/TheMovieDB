363e5e289a57915803c320fcc5ce538c7e10c551
Ashish Badak <ashbadak@gmail.com>
2020-10-08 16:08:28 +0530

    added MovieListingViewController file and scene

---
+++ TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListingViewController.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 08/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class MovieListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

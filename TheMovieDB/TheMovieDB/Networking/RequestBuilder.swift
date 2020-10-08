926065f20ccdd47c8f5df85eb9567d01fb3d9c1b
Ashish Badak <ashbadak@gmail.com>
2020-10-09 00:42:16 +0530

    created default RequestBuilder

--- TheMovieDB/TheMovieDB/Networking/RequestBuilder.swift
+++ TheMovieDB/TheMovieDB/Networking/RequestBuilder.swift
@@ -11,3 +11,37 @@ import Foundation
 protocol URLRequestConvertible {
     func asURLRequest() -> URLRequest?
 }
+
+struct RequestBuilder: URLRequestConvertible {
+    let endPoint: RequestEndPoint
+    let cachePolicy: URLRequest.CachePolicy
+    let timeoutInterval: TimeInterval
+    
+    func asURLRequest() -> URLRequest? {
+        var urlRequest = URLRequest(
+            url: endPoint.baseURL,
+            cachePolicy: cachePolicy,
+            timeoutInterval: timeoutInterval
+        )
+        
+        urlRequest.httpMethod = endPoint.httpMethod.rawValue
+        urlRequest.allHTTPHeaderFields = endPoint.getHeaders()
+        
+        do {
+            try endPoint.encoder.encode(urlRequest: &urlRequest, with: endPoint.parameters)
+        } catch {
+            /// - Note: We can throw an error from here as well if required
+            debugPrint("Failed to encode URL")
+        }
+        
+        return urlRequest
+    }
+}
+
+extension RequestBuilder {
+    init(endPoint: RequestEndPoint) {
+        cachePolicy = .reloadIgnoringLocalAndRemoteCacheData
+        timeoutInterval = 10.0
+        self.endPoint = endPoint
+    }
+}

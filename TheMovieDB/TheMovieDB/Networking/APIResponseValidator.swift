a9ffec21c93cb78dce4c9c8be17ba08bf97092b2
Ashish Badak <ashbadak@gmail.com>
2020-10-09 01:25:46 +0530

    created APIResponseValidator

--- TheMovieDB/TheMovieDB/Networking/APIResponseValidator.swift
+++ TheMovieDB/TheMovieDB/Networking/APIResponseValidator.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+protocol APIResponseValidator {
+    func validate(response: HTTPURLResponse) -> Result<Bool, APIRequestError>
+}
+
+extension APIResponseValidator {
+    func validate(response: HTTPURLResponse) -> Result<Bool, APIRequestError> {
+        switch response.statusCode {
+        case 200...299: return .success(true)
+        case 401...500: return .failure(.authenticationError)
+        case 501...599: return .failure(.badRequest)
+        case 600: return .failure(.outdated)
+        default: return .failure(.failed)
+        }
+    }
+}
+
+struct DefaultAPIResponseValidator: APIResponseValidator { }

3ea1aa2c831747da465bb9b959c97037295d2388
Ashish Badak <ashbadak@gmail.com>
2020-10-08 20:02:12 +0530

    created URLParameterEncoder

--- TheMovieDB/TheMovieDB/Networking/Encoding/URLParameterEncoder.swift
+++ TheMovieDB/TheMovieDB/Networking/Encoding/URLParameterEncoder.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct URLParameterEncoder: ParameterEncoder {
+    func encode(urlRequest: inout URLRequest, with parameters: Parameters) throws {
+        guard let url = urlRequest.url else {
+            throw NetworkError.missingURL
+        }
+        
+        guard parameters.isEmpty else {
+            return
+        }
+        
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return
+        }
+        
+        urlComponents.queryItems = parameters.asURLQueryItems
+        urlRequest.url = urlComponents.url
+    }
+}

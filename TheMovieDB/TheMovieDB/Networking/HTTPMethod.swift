b3dac2155be8ec026ee14a37bc20d538215f42ed
Ashish Badak <ashbadak@gmail.com>
2020-10-08 16:20:05 +0530

    created HTTPMethod enum

---
+++ TheMovieDB/TheMovieDB/Networking/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 08/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}

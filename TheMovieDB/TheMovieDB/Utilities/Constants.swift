2a81ecab0692cfbcaa8b0daaff519c416c34ef35
Ashish Badak <ashbadak@gmail.com>
2020-10-09 17:55:07 +0530

    added constants required for api query

---
+++ TheMovieDB/TheMovieDB/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 09/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static var apiKey: String { "" }
+    static var contnetLanguage: String { "en-US" }
+}

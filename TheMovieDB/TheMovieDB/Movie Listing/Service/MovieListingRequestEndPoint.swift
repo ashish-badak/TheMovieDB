ce085ed399e89fcc94aa559b8200e58f161ab4f6
Ashish Badak <ashbadak@gmail.com>
2020-10-09 18:21:04 +0530

    created MovieListingRequestEndPoint

--- TheMovieDB/TheMovieDB/Movie Listing/Service/MovieListingRequestEndPoint.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Service/MovieListingRequestEndPoint.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct MovieListingRequestEndPoint: GetRequestEndPoint {
+    var path: String { "movie/now_playing" }
+    var extraHeaders: HTTPHeaders?
+        
+    var parameters: Parameters { queryParams.getQueryParameters() }
+    
+    var page: Int
+    var queryParams: PaginatedQueryParamsProvider {
+        .init(page: page)
+    }
+}

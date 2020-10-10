1031c39c844753300d7fd376adad4f93eecdb091
Ashish Badak <ashbadak@gmail.com>
2020-10-10 19:44:47 +0530

    MovieCardTableViewCell conforms to NibLoadableView and disabled selectionStyle

--- TheMovieDB/TheMovieDB/Movie Listing/Views/MovieCardTableViewCell.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Views/MovieCardTableViewCell.swift
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class MovieCardTableViewCell: UITableViewCell {
+class MovieCardTableViewCell: UITableViewCell, NibLoadableView {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureView()
     }
     
+    func configureView() {
+        selectionStyle = .none
+    }
 }

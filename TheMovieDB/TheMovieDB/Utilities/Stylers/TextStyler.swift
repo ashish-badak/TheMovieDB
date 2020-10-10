1c4fa9c4a81de8324402c5ecb1e196d1e1a15cd5
Ashish Badak <ashbadak@gmail.com>
2020-10-11 01:35:32 +0530

    created text styler

---
+++ TheMovieDB/TheMovieDB/Utilities/Stylers/TextStyler.swift
@@ -0,0 +1,35 @@
+//
+//  TextStyler.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 11/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+struct TextStyle {
+    let font: UIFont
+    let color: UIColor
+}
+
+enum TextStyler {
+    static var title: TextStyle {
+        .init(font: .systemFont(ofSize: 16), color: UIColor.Font.title)
+    }
+    
+    static var subtitle: TextStyle {
+        .init(font: .systemFont(ofSize: 14), color: UIColor.Font.subtitle)
+    }
+    
+    static var normal: TextStyle {
+        .init(font: .systemFont(ofSize: 12), color: UIColor.Font.listing)
+    }
+}
+
+extension UILabel {
+    static func stylise(_ label: UILabel, textStyle: TextStyle) {
+        label.font = textStyle.font
+        label.textColor = textStyle.color
+    }
+}

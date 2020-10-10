0f88f424a40b09e43ac2e9a3bd0884a99958257f
Ashish Badak <ashbadak@gmail.com>
2020-10-11 03:14:25 +0530

    constructed view models and displayed movie list on movie list content view controller

--- TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingContentViewController.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingContentViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class MovieListingContentViewController: UIViewController {
     var movieList: ResponseList<Movie>
+    var movieListViewModel = [MovieCardViewModeling]()
     
     private var tableView: UITableView = {
         let tableView = UITableView()
         tableView.showsHorizontalScrollIndicator = false
         tableView.showsVerticalScrollIndicator = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .none
         return tableView
     }()
     
@@ -32,5 +34,38 @@ class MovieListingContentViewController: UIViewController {
         super.viewDidLoad()
         view.addSubview(tableView)
         tableView.layoutConstraints(superView: self.view)
+        tableView.register(MovieCardTableViewCell.self)
+        tableView.dataSource = self
+        updateViewModels(movieList: movieList.results)
+    }
+    
+    func updateMovieList(movieList: ResponseList<Movie>) {
+        self.movieList.results.append(contentsOf: movieList.results)
+        updateViewModels(movieList: movieList.results)
+    }
+    
+    func updateViewModels(movieList: [Movie]) {
+        var viewModels = [MovieCardViewModeling]()
+        for movie in movieList {
+            let vm = MovieCardViewModel(movie: movie)
+            vm.bookButtonActionHandler = {
+                /// - TODO: handle book button callback
+            }
+            viewModels.append(vm)
+        }
+        movieListViewModel.append(contentsOf: viewModels)
+        tableView.reloadData()
+    }
+}
+
+extension MovieListingContentViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: MovieCardTableViewCell = tableView.dequeue(forIndexPath: indexPath)
+        cell.setData(movieListViewModel[indexPath.row])
+        return cell
     }
 }

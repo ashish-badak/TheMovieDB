a0b430b3b02c8a3c55725dc2515ac0012c510846
Ashish Badak <ashbadak@gmail.com>
2020-10-10 16:51:51 +0530

    added initialisation functions with Movie list property

--- TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingContentViewController.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingContentViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class MovieListingContentViewController: UIViewController {
+    var movieList: ResponseList<Movie>
+    
+    init(movieList: ResponseList<Movie>) {
+        self.movieList = movieList
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

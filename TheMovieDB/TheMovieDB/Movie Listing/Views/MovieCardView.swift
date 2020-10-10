0af0c1fb95149c0cb936e2cf47fc0f3ef7b077c3
Ashish Badak <ashbadak@gmail.com>
2020-10-11 00:17:23 +0530

    configured MovieCardView with data setup

--- TheMovieDB/TheMovieDB/Movie Listing/Views/MovieCardView.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Views/MovieCardView.swift
@@ -18,9 +18,35 @@ class MovieCardView: UIView, NibFileOwnerLoadbleView {
     
     @IBOutlet weak var bookNowButton: UIButton!
     
+    weak var viewModel: MovieCardViewModelDelegate?
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         loadFromNib()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureView()
+    }
+    
+    private func configureView() {
+        bookNowButton.setTitleColor(.white, for: .normal)
+        bookNowButton.backgroundColor = .blue
+        bookNowButton.addTarget(self, action: #selector(bookButtonTapped), for: .touchUpInside)
+    }
+    
+    func setData(_ viewModel: MovieCardViewModeling) {
+        self.viewModel = viewModel
+        titleLabel.text = viewModel.title
+        releaseDateLabel.text = viewModel.releaseDate
+        overviewLabel.text = viewModel.overview
+        bookNowButton.setTitle(viewModel.bookButtonTitle, for: .normal)
+    }
+    
+    @objc func bookButtonTapped() {
+        print("Book Tapped")
+        viewModel?.bookButtonActionHandler?()
     }
 }
 

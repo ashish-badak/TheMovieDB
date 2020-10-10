279aeefeab99a70c7c7c8f1cd3a8a385040f2cde
Ashish Badak <ashbadak@gmail.com>
2020-10-10 12:17:15 +0530

    using ApplicationCoordinator to load initial controller in app delegate

--- TheMovieDB/TheMovieDB/Application Startup/AppDelegate.swift
+++ TheMovieDB/TheMovieDB/Application Startup/AppDelegate.swift
@@ -10,14 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
+    var applicationCoordinator: ApplicationCoordinator?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow()
-        window?.rootViewController = ViewController.instantiate()
-        window?.makeKeyAndVisible()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let applicationCoordinator = ApplicationCoordinator(window: window)
+        applicationCoordinator.start()
+        
+        self.window = window
+        self.applicationCoordinator = applicationCoordinator
         
         return true
     }

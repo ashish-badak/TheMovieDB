29c9523231e8a2c82778c2e33b872b8d72ff3e78
Ashish Badak <ashbadak@gmail.com>
2020-10-10 15:36:24 +0530

    created ErrorViewController

---
+++ TheMovieDB/TheMovieDB/Error State/ErrorViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ErrorViewController.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 10/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class ErrorViewController: UIViewController {
+    private var errorMessage: String
+    
+    private var errorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    init(errorMessage: String) {
+        self.errorMessage = errorMessage
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setErrorLabel()
+    }
+    
+    private func setErrorLabel() {
+        errorLabel.text = errorMessage
+        view.addSubview(errorLabel)
+        errorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        errorLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}

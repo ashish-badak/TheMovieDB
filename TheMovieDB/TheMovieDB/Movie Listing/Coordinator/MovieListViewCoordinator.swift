49800bbc1a567ab29fa1c60317fb85ff3fcdf8aa
Ashish Badak <ashbadak@gmail.com>
2020-10-10 12:15:57 +0530

    created MovieListViewCoordinator

--- TheMovieDB/TheMovieDB/Movie Listing/Coordinator/MovieListViewCoordinator.swift
+++ TheMovieDB/TheMovieDB/Movie Listing/Coordinator/MovieListViewCoordinator.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Ashish Badak. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class MovieListViewCoordinator: Coordinator {
+    private let presenter: UINavigationController
+    private var movieListViewController: MovieListingViewController?
+    
+    init(presenter: UINavigationController) {
+        self.presenter = presenter
+    }
+    
+    func start() {
+        let movieListViewController = MovieListingViewController.instantiate()
+        movieListViewController.title = "Now Playing"
+        presenter.pushViewController(movieListViewController, animated: true)
+        
+        self.movieListViewController = movieListViewController
+    }
+}

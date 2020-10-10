be9c5b2f8e80dc54a5ad6d3cfaa189dc01a83f96
Ashish Badak <ashbadak@gmail.com>
2020-10-10 16:38:16 +0530

    added MovieListingContentViewController file

---
+++ TheMovieDB/TheMovieDB/Movie Listing/Controller/MovieListingContentViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieListingContentViewController.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 10/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class MovieListingContentViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

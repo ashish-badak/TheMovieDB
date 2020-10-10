d93ed2f06c77ed00ac3df7b565712866252128d0
Ashish Badak <ashbadak@gmail.com>
2020-10-10 19:21:51 +0530

    added movie card cell file

---
+++ TheMovieDB/TheMovieDB/Movie Listing/Views/MovieCardView.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCardView.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 10/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class MovieCardView: UIView {
+    
+}

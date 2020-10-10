c3899195a8817f553c1c1cd37e0697223eac0825
Ashish Badak <ashbadak@gmail.com>
2020-10-10 11:59:56 +0530

    created Coordinator protocol

---
+++ TheMovieDB/TheMovieDB/Application Navigation/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 10/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}

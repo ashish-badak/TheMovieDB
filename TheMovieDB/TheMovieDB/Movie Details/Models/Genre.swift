ea73d5a88e7fea7a3ed2394c28e8a3ae2c228bc9
Ashish Badak <ashbadak@gmail.com>
2020-10-12 01:47:27 +0530

    created Genre model

---
+++ TheMovieDB/TheMovieDB/Movie Details/Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 12/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
+
+struct Genre {
+    let id: Int
+    let name: String
+}

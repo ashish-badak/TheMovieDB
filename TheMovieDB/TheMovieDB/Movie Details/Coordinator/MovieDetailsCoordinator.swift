efad749a0b8389bac49cc0e3c3e2c6e24e85484b
Ashish Badak <ashbadak@gmail.com>
2020-10-11 12:16:34 +0530

    added MovieDetailsCoordinator file

---
+++ TheMovieDB/TheMovieDB/Movie Details/Coordinator/MovieDetailsCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDetailsCoordinator.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 11/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation

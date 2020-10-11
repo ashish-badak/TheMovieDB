2533c8f8e2b7e20b7b5a1b22902956a7ee5aae18
Ashish Badak <ashbadak@gmail.com>
2020-10-12 01:14:48 +0530

    making API calls and sending state update callbacks from movie details

--- TheMovieDB/TheMovieDB/Movie Details/Controller/MovieDetailsViewController.swift
+++ TheMovieDB/TheMovieDB/Movie Details/Controller/MovieDetailsViewController.swift
@@ -17,7 +17,26 @@ protocol MovieDetailsContainerDelegate: AnyObject {
 class MovieDetailsViewController: UIViewController, StoryboardInstantiable {
     static var storyboardProvider: StoryboardProvider { Storyboard.movieDetails }
 
+    var movieId: Int?
+    var contentProvider: MovieDetailsContentProvider?
+    
+    weak var delegate: MovieDetailsContainerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let id = movieId {
+            fetchContent(movieId: id)
+        } else {
+            delegate?.setErrorState(errorMessage: "Something went wrong!")
+        }
+    }
+    
+    func fetchContent(movieId: Int) {
+        self.delegate?.didStartContentFetching()
+        contentProvider = MovieDetailsContentProvider(movieId: movieId)
+        contentProvider?.fetchContent(completion: { [weak self] (result) in
+            self?.delegate?.didFetchContent(result: result)
+        })
     }
 }

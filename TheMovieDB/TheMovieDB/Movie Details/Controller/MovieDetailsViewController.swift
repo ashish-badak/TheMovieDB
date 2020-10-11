e29161882e5d350c87bd06fb27d2512644a65166
Ashish Badak <ashbadak@gmail.com>
2020-10-11 12:51:54 +0530

    made movie details controller storyboard instantiable

--- TheMovieDB/TheMovieDB/Movie Details/Controller/MovieDetailsViewController.swift
+++ TheMovieDB/TheMovieDB/Movie Details/Controller/MovieDetailsViewController.swift
@@ -8,23 +8,10 @@
 
 import UIKit
 
-class MovieDetailsViewController: UIViewController {
+class MovieDetailsViewController: UIViewController, StoryboardInstantiable {
+    static var storyboardProvider: StoryboardProvider { Storyboard.movieDetails }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

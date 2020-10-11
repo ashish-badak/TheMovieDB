9e4aefc275492ca7773265466110a55dbe892b94
Ashish Badak <ashbadak@gmail.com>
2020-10-11 20:16:43 +0530

    added MovieDetailsContentViewController file

---
+++ TheMovieDB/TheMovieDB/Movie Details/Controller/MovieDetailsContentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailsContentViewController.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 11/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

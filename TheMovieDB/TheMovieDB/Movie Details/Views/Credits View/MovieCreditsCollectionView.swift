8ad324241b5f147fd13e80be047c203b3d2b1f0e
Ashish Badak <ashbadak@gmail.com>
2020-10-12 12:48:16 +0530

    configured collection view

--- TheMovieDB/TheMovieDB/Movie Details/Views/Credits View/MovieCreditsCollectionView.swift
+++ TheMovieDB/TheMovieDB/Movie Details/Views/Credits View/MovieCreditsCollectionView.swift
@@ -26,7 +26,38 @@ class MovieCreditsCollectionView: UICollectionView {
     }
     
     func configureView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = .init(top: 8, left: 16, bottom: 8, right: 16)
+        layout.minimumInteritemSpacing = 16
+        layout.itemSize = .init(width: UIScreen.main.bounds.width - 48, height: 80)
+            
+        collectionViewLayout = layout
+        
+        showsHorizontalScrollIndicator = false
+        register(MovieCreditsCollectionViewCell.self)
+        
+        dataSource = self
+    }
+    
     func setData(creditViewModels: [CastViewModel]) {
         viewModels = creditViewModels
+        reloadData()
+    }
+}
+
+extension MovieCreditsCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModels?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: MovieCreditsCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+        
+        if let vm = viewModels?[indexPath.item] {
+            cell.setData(viewModel: vm)
+        }
+        
+        return cell
     }
 }

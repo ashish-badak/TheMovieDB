4181b92e6802dcc8cc9906091b10aadbc756d883
Ashish Badak <ashbadak@gmail.com>
2020-10-12 10:10:53 +0530

    added movie credits view model

---
+++ TheMovieDB/TheMovieDB/Movie Details/View Models/MovieCreditsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieCreditsViewModel.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 12/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation

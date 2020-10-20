bfbc83d5507fa0658ea0093010904a262202d1fd
Ashish Badak <ashbadak@gmail.com>
2020-10-20 19:22:47 +0530

    designed movie review view

--- TheMovieDB/TheMovieDB/Movie Details/Views/Reviews/MovieReviewCollectionViewCell.swift
+++ TheMovieDB/TheMovieDB/Movie Details/Views/Reviews/MovieReviewCollectionViewCell.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
-class MovieReviewCollectionViewCell: UICollectionViewCell {
+final class MovieReviewCollectionViewCell: UICollectionViewCell {
+    private let mainView: MovieReviewView = {
+        let view = MovieReviewView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        addSubview(mainView)
+        mainView.layoutConstraints(superView: self)
+    }
+    
+    func setData(_ viewModel: MovieReviewViewModel) {
+        mainView.setData(viewModel)
+    }
 }
--- TheMovieDB/TheMovieDB/Movie Details/Views/Reviews/MovieReviewView.swift
+++ TheMovieDB/TheMovieDB/Movie Details/Views/Reviews/MovieReviewView.swift
@@ -9,13 +9,63 @@
 import UIKit
 
 class MovieReviewView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        stackView.addArrangedSubview(authorLabel)
+        stackView.addArrangedSubview(descriptionLabel)
+        addSubview(stackView)
+        stackView.layoutConstraints(
+            superView: self,
+            topConstant: 8,
+            leadingConstant: 12,
+            bottomConstant: 8,
+            trailingConstant: 12
+        )
+        
+        setBorder(
+            color: UIColor.Border.primary,
+            size: 1,
+            cornerRadius: 5
+        )
+        
+        setupLabels()
+    }
+    
+    private func setupLabels() {
+        UILabel.stylise(authorLabel, textStyle: TextStyler.reviewTitle)
+        UILabel.stylise(descriptionLabel, textStyle: TextStyler.reviewDescription)
+    }
+    
+    func setData(_ viewModel: MovieReviewViewModel) {
+        authorLabel.text = viewModel.author
+        descriptionLabel.text = viewModel.reviewDescription
+    }
 }

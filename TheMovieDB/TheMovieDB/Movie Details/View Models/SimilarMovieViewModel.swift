77086e778b3ebeaa62f8fbeaac522fc437599f0d
Ashish Badak <ashbadak@gmail.com>
2020-10-20 16:26:42 +0530

    created similar movie view model

---
+++ TheMovieDB/TheMovieDB/Movie Details/View Models/SimilarMovieViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  SimilarMovieViewModel.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 20/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
+
+final class SimilarMovieViewModel: RowViewModel {
+    let posterURL: String?
+    
+    init(movie: Movie) {
+        posterURL = PosterImageURLBuilder(scale: .small).getImageURL(imagePath: movie.posterPath)
+    }
+}

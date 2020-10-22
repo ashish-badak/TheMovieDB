9e7ee3ce2cb2ae11f9c8650192f4b533d5e4ebd6
Ashish Badak <ashbadak@gmail.com>
2020-10-22 23:50:09 +0530

    created trie with functionality to insert word for indexing and get suggestions for a word

--- TheMovieDB/TheMovieDB/Search/Data Indexing/Trie.swift
+++ TheMovieDB/TheMovieDB/Search/Data Indexing/Trie.swift
@@ -7,3 +7,74 @@
 //
 
 import Foundation
+
+final class Trie {
+    typealias Node = TrieNode<Character>
+    
+    fileprivate let root: Node
+    
+    init() {
+        root = Node()
+    }
+    
+    func insert(word: String, forId id: Int) {
+        guard !word.isEmpty else { return }
+        
+        let words = word.components(separatedBy: " ")
+        
+        for currentWord in words {
+            var currentNode = root
+            
+            for character in currentWord.lowercased() {
+                if let child = currentNode.children[character] {
+                    currentNode = child
+                    currentNode.setWordId(id)
+                } else {
+                    currentNode.add(child: character, id: id)
+                    currentNode = currentNode.children[character]!
+                }
+            }
+        }
+    }
+    
+    func findLastNode(ofWord word: String) -> Node? {
+        guard !word.isEmpty else { return nil }
+        
+        var currentNode = root
+        
+        for character in word.lowercased() {
+            if let child = currentNode.children[character] {
+                currentNode = child
+            } else {
+                return nil
+            }
+        }
+        
+        return currentNode
+    }
+    
+    func getMovieSuggestions(forWord word: String) -> [Int] {
+        guard !word.isEmpty else { return [] }
+        
+        var suggestions = Set<Int>()
+        
+        let words = word.components(separatedBy: " ")
+        for (index, currentWord) in words.enumerated() {
+            guard !currentWord.isEmpty else {
+                continue
+            }
+
+            guard let lastNode = findLastNode(ofWord: currentWord) else {
+                return []
+            }
+            
+            if index == 0 {
+                suggestions.formUnion(lastNode.ids)
+            } else {
+                suggestions.formIntersection(lastNode.ids)
+            }
+        }
+        
+        return Array(suggestions)
+    }
+}

733baa7b23fa3b313726c1bdaa1a2535335a21d1
Ashish Badak <ashbadak@gmail.com>
2020-10-22 23:38:09 +0530

    added files required for movie indexing

---
+++ TheMovieDB/TheMovieDB/Search/Data Indexing/Trie.swift
@@ -0,0 +1,9 @@
+//
+//  Trie.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 22/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation
---
+++ TheMovieDB/TheMovieDB/Search/Data Indexing/TrieNode.swift
@@ -0,0 +1,9 @@
+//
+//  TrieNode.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 22/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import Foundation

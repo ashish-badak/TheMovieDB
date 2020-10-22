fde1382fa8a50c77c91918039cb2b347508fd679
Ashish Badak <ashbadak@gmail.com>
2020-10-22 20:59:15 +0530

    added RecentlySearchedMovieTableViewCell file

---
+++ TheMovieDB/TheMovieDB/Search/Views/RecentlySearchedMovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecentlySearchedMovieTableViewCell.swift
+//  TheMovieDB
+//
+//  Created by Ashish Personal on 22/10/20.
+//  Copyright © 2020 Ashish Badak. All rights reserved.
+//
+
+import UIKit
+
+class RecentlySearchedMovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

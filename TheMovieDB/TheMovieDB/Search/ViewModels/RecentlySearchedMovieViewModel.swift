00c97994151eb35d6b6817bc24fe7f25edc4d75a
Ashish Badak <ashbadak@gmail.com>
2020-10-22 23:53:29 +0530

    created RecentlySearchedSectionViewModel

--- TheMovieDB/TheMovieDB/Search/ViewModels/RecentlySearchedMovieViewModel.swift
+++ TheMovieDB/TheMovieDB/Search/ViewModels/RecentlySearchedMovieViewModel.swift
@@ -11,9 +11,37 @@ import Foundation
 final class RecentlySearchedMovieViewModel: RowViewModel {
     let posterURL: String?
     let title: String
+    var didSelect: (() -> Void)?
     
     init(movie: Movie) {
         title = movie.title
         posterURL = PosterImageURLBuilder(scale: .small).getImageURL(imagePath: movie.posterPath)
+    }
+}
+
+final class RecentlySearchedSectionViewModel: SectionViewModel {
+    var rowViewModels: [RecentlySearchedMovieViewModel]
+    let sectionHeaderViewModel: HeadingViewModel
+    var didSelectMovie: ((Movie) -> Void)?
+    
+    init(movies: [Movie]) {
+        sectionHeaderViewModel = HeadingViewModel(heading: "Recently Searched")
+        
+        rowViewModels = []
+        for movie in movies {
+            let viewModel = RecentlySearchedMovieViewModel(movie: movie)
+            viewModel.didSelect = { [weak self] in
+                self?.didSelectMovie?(movie)
+            }
+            rowViewModels.append(viewModel)
+        }
+    }
+    
+    func getRowViewModels() -> [RowViewModel] {
+        rowViewModels
+    }
+    
+    func getSectionHeaderViewModel() -> RowViewModel? {
+        sectionHeaderViewModel
     }
 }

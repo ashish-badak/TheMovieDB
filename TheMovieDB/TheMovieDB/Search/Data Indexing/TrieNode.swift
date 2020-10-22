abf955e379b7662a3af648f73a7545d322a2ec0c
Ashish Badak <ashbadak@gmail.com>
2020-10-22 23:41:39 +0530

    created TrieNode data structure class

--- TheMovieDB/TheMovieDB/Search/Data Indexing/TrieNode.swift
+++ TheMovieDB/TheMovieDB/Search/Data Indexing/TrieNode.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+final class TrieNode<T: Hashable> {
+    var value: T?
+    var children: [T: TrieNode] = [:]
+    var ids = Set<Int>()
+    
+    init(value: T? = nil) {
+        self.value = value
+    }
+    
+    func add(child: T, id: Int) {
+        defer { children[child]?.setWordId(id) }
+        
+        guard children[child] == nil else {
+            return
+        }
+        
+        children[child] = TrieNode(value: child)
+    }
+    
+    func setWordId(_ id: Int) {
+        ids.insert(id)
+    }
+}
